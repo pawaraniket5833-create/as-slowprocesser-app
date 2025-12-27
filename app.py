73546a83cdea1bf2c5692c0a5471de9e62d9c998
pawaraniket5833-create <pawaraniket5833@gmail.com>
2025-12-27 21:36:55 +0530

    add app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AS.SLOWPROCESSER",
+    layout="centered"
+)
+
+st.title("AS.SLOWPROCESSER 🚀")
+st.write("Innovation • Smart Home • Tech Projects")
+
+if st.button("Start App"):
+    st.success("Your app is running successfully!")
